eefc2550f6f8046c0e1e773dad7fcd15b4de6fd3
Daksh Patel <daksh6928@gmail.com>
2024-10-29 22:05:15 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll on nav link click
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
